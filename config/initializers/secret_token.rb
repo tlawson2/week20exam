01e8ccd07486192ac0e40a9d145c971525b27e3c
Tasha Lawson <tlawson@chicagotechacademy.org>
2014-01-22 13:15:26 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week20exam::Application.config.secret_key_base = 'cd28fcc70981db823e3d71329e1b73fb66b0db7c35d03fdbcbe2d20854eadae2d4a01b50e6c9843bb870c1bd245bd1e7bf5fee7b5e550476ab36495b8dbde22f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Week20exam::Application.config.session_store :cookie_store, key: '_week20exam_session'
